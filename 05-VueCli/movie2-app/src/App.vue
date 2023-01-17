b5ef42f26ec6db1c591ff4b28d5c61281f8d97a6
Sidney François <sidney.fvm.ps@outlook.com>
2023-01-17 08:09:03 -0300

    Add second project of vue-cli

---
+++ 05-VueCli/movie2-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>{{ title }}</h2>
+  <Second />
+</template>
+
+<script>
+import Second from './components/Second.vue';
+
+export default {
+  name: 'App',
+  components: { Second },
+  data() {
+    return {
+      title: 'Title from the App.vue'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 05-VueCli/movie2-app/src/components/Second.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>{{ name }}</h2>
+</template>
+
+<script>
+  export default {
+    name: 'Second',
+    data() {
+      return {
+        name: 'Name from the second'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
